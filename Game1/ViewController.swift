9fb97b131e997b99e8bbf5c9861d4d24f61f6a9d
brusik666 <dmytrobrusov@gmail.com>
2022-11-04 11:48:37 +0200

    Initial Commit

---
+++ Game1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Game1
+//
+//  Created by Brusik on 04.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
